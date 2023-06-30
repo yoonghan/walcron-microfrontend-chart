d5e6c2e07d50af720624da855ecde03d9ddb8c30
han <walcoorperation@gmail.com>
2023-06-30 12:50:41 +0800

    Initial commit

---
+++ src/bootstrap.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { createRouter } from "./routing/router-factory";
+import { RoutingStrategy } from "./routing/types";
+import "./index.css";
+
+const mount = ({
+  mountPoint,
+  initialPathname,
+  routingStrategy,
+}: {
+  mountPoint: HTMLElement;
+  initialPathname?: string;
+  routingStrategy?: RoutingStrategy;
+}) => {
+  const router = createRouter({ strategy: routingStrategy, initialPathname });
+  const root = createRoot(mountPoint);
+  root.render(<RouterProvider router={router} />);
+  return () => queueMicrotask(() => root.unmount());
+};
+
+export { mount };
